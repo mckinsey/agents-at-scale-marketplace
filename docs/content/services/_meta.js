4f1fb560cc22d4a9e1eda4209d9e2385f79c6564
Kristof Havasi <chris.hvse@gmail.com>
2026-02-26 09:40:55 +0000

    feat: langchain executor update docs

--- docs/content/services/_meta.js
+++ docs/content/services/_meta.js
@@ -1,6 +1,7 @@
 export default {
   'a2a-inspector': 'A2A Inspector',
   'ark-sandbox': 'ARK Sandbox',
+  'executor-langchain': 'LangChain Executor',
   'file-gateway': 'File Gateway',
   langfuse: 'Langfuse',
   'mcp-inspector': 'MCP Inspector',
